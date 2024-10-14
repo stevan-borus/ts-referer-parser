ddf26193c43eb2062fb11cb5589709fda60ae199
Stevan Borus <steva.borus@gmail.com>
2024-10-14 12:47:21 +0200

    fix types

--- src/types.ts
+++ src/types.ts
@@ -1,86 +1,22 @@
-export type Medium =
-  | 'search'
-  | 'social'
-  | 'email'
-  | 'video'
-  | 'paid'
-  | 'internal'
+import type refererData from './utils/latest-referers.json';
+
+type RefererCategories = keyof typeof refererData;
+type RefererSources = {
+  [K in RefererCategories]: keyof (typeof refererData)[K];
+}[RefererCategories];
+
+export type RefererCategory =
+  | RefererCategories
   | 'unknown'
+  | 'internal'
   | 'invalid'
   | 'direct'
   | (string & {});
 
-type RefererSource =
-  | 'Google'
-  | 'Yandex Maps'
-  | 'Yahoo!'
-  | 'TalkTalk'
-  | '1.cz'
-  | 'Softonic'
-  | 'GAIS'
-  | 'Freecause'
-  | '360.cn'
-  | 'RPMFind'
-  | 'Comcast'
-  | 'Voila'
-  | 'Nifty'
-  | 'Atlas'
-  | 'Lo.st'
-  | 'DasTelefonbuch'
-  | 'Fireball'
-  | '1und1'
-  | 'Virgilio'
-  | 'Telstra'
-  | 'Web.nl'
-  | 'Plazoo'
-  | 'Goyellow.de'
-  | 'AOL'
-  | 'Acoon'
-  | 'Free'
-  | 'Apollo Latvia'
-  | 'HighBeam'
-  | 'I-play'
-  | 'FriendFeed'
-  | 'Yasni'
-  | 'Gigablast'
-  | 'Arcor'
-  | 'arama'
-  | 'Fixsuche'
-  | 'Apontador'
-  | 'Search.com'
-  | 'Monstercrawler'
-  | 'Google Images'
-  | 'ABCsøk'
-  | 'Google Product Search'
-  | 'Blogpulse'
-  | 'Hooseek.com'
-  | 'Dalesearch'
-  | 'Alice Adsl'
-  | 'T-Online'
-  | 'Sogou'
-  | 'Hit-Parade'
-  | 'SearchCanvas'
-  | 'Jungle Key'
-  | 'Interia'
-  | 'Genieo'
-  | 'Tiscali'
-  | 'Gomeo'
-  | 'Facebook'
-  | 'Twitter'
-  | 'LinkedIn'
-  | 'Instagram'
-  | 'Pinterest'
-  | 'YouTube'
-  | 'Reddit'
-  | 'Tumblr'
-  | 'WhatsApp'
-  | 'Telegram'
-  | 'Snapchat'
-  | 'TikTok'
-  | (string & {});
+export type RefererSource = RefererSources | (string & {});
 
 export type Referer = {
-  medium: Medium;
+  medium: RefererCategory;
   referer: RefererSource | null;
   term: string | null;
 };
